614021b2fc4df766e8c799b7011c3b749bf94fbc
TheChickenIsOnFire <jmoore@dolgeville.org>
2025-04-09 16:14:49 +0000

    Fixes

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "closeWindow") {
+    chrome.windows.getCurrent(window => {
+      chrome.windows.remove(window.id);
+    });
+  }
+});
+
 chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL("sandbox.html") });
+  chrome.windows.create({
+    url: chrome.runtime.getURL('sandbox.html'),
+    type: 'popup',
+    width: 1280,
+    height: 720,
+    left: 100,
+    top: 100
+  });
 });
