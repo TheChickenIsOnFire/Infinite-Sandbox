82c2e119fe387d6941768ee9c842fcd614289097
TheChickenIsOnFire <jmoore@dolgeville.org>
2025-04-09 12:43:26 +0000

    Changes

--- menu.js
+++ menu.js
@@ -1,3 +1,13 @@
 document.getElementById('exitButton').addEventListener('click', () => {
   window.close();
 });
+
+document.getElementById('optionsButton').addEventListener('click', () => {
+  document.querySelector('.menu').style.display = 'none';
+  document.querySelector('.options-menu').style.display = 'block';
+});
+
+document.getElementById('backButton').addEventListener('click', () => {
+  document.querySelector('.options-menu').style.display = 'none';
+  document.querySelector('.menu').style.display = 'block';
+});
